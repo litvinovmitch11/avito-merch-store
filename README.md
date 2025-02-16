7e816a1e1dc387983a8338c224cf9fa71400869a
Michael Litvinov <mihaillitvinov26324@gmail.com>
2025-02-16 18:58:15 +0000

    add server host port

--- README.md
+++ README.md
@@ -1,1 +1,6 @@
 # Test assignment - Avito backend internship
+
+# Как запустить? 
+* Клонируете репозиторий
+* В файл `secrets/.env ` сохраняете секреты для PostgreSQL, JWT и прочие
+* Запускаете `make all`
--- internal/cmd/root.go
+++ internal/cmd/root.go
@@ -1,9 +1,11 @@
 package cmd
 
 import (
+	"fmt"
 	"log"
 	"net/http"
 	"os"
+	"strconv"
 
 	"github.com/go-chi/chi/v5"
 	"github.com/litvinovmitch11/avito-merch-store/internal/generated/api"
@@ -18,9 +20,12 @@ func Run() {
 	r := chi.NewMux()
 	h := api.HandlerFromMux(server, r)
 
+	host := os.Getenv("HOST")
+	port, _ := strconv.Atoi(os.Getenv("PORT"))
+
 	s := &http.Server{
 		Handler: h,
-		Addr:    "localhost:8080",
+		Addr:    fmt.Sprintf("%s:%d", host, port),
 	}
 
 	log.Fatal(s.ListenAndServe())
